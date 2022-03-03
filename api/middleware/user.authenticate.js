f0b228158c7ea28d4288e13a4471f3c946adb444
Okonkwo Maryann Chidimma <okonkwo20chidimma@gmail.com>
2022-03-03 08:55:47 +0100

    fixed merge conflict

--- api/middleware/user.authenticate.js
+++ api/middleware/user.authenticate.js
@@ -6,15 +6,12 @@ const CustomError = require("../utils/custom.error")
 exports.authenticate = async (req, res, next) => {
 
     const token = req.headers["x-auth-token"]
-    console.log(token)
+
     if (!token) throw new CustomError("Not Authorized access", 401)
-    try {
-        const decodedUser = verifyToken(token)
-        const user = await userService.findByEmail(decodedUser.email)
-        if (!user) throw new CustomError("User does not exist", 401)
-        next()
-    }
-    catch (err) {
-        next(err)
-    }
+
+    const decodedUser = verifyToken(token)
+    const user = await userService.findByEmail(decodedUser.email)
+    if (!user) throw new CustomError("User does not exist", 401)
+
+    next(err)
 } 
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
+const { authenticate } = require('../middleware/user.authenticate');
 require('express-async-errors');
 
 const router = express.Router();
 
-router.post("/", userController.create);
+router.post("/", authenticate, userController.create);
 router.get("/", userController.getAll);
 router.patch("/:blogId", userController.update);
 router.get("/getbyid", userController.getOne);
